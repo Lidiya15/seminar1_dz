039ef55c7301a51098152b51750b477b9f805c8e
Lidiya <lidia.pahno@yandex.ru>
2022-11-01 03:34:58 +0300

    add solution 8 tasks

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// 8 Напишите программу, которая на вход принимает число N, а на выходе показывает 
+// все четные числа от 1 до N.
+
+Console.WriteLine("Введите натуральное число");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count <= x)
+{
+   Console.Write(count + " ");
+   count = count + 2;
+}
+Console.WriteLine();
+
+
