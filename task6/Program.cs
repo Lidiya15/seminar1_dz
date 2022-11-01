28780ee673b4dae9fd5a80e8945f387ce7c74648
Lidiya <lidia.pahno@yandex.ru>
2022-11-01 03:25:41 +0300

    add solution 6 tasks

---
+++ task6/Program.cs
@@ -0,0 +1,15 @@
+﻿// 6 Напишите программу , которая на вход принимает число и выдает, является ли число четным ( делится ли оно
+// на 2 без остатка)
+
+Console.WriteLine("Введите натуральное число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int x = numberA % 2;
+if(x == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+
